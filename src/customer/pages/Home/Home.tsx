f3c93343b71ca1ca76b1408347eafc2be8ad2740
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2025-05-22 10:23:15 +0530

    home page update

--- src/customer/pages/Home/Home.tsx
+++ src/customer/pages/Home/Home.tsx
@@ -30,10 +30,7 @@ const Home = () => {
     return (
         <>
             {(!homePage.loading) ? <div className='space-y-5 lg:space-y-10 relative'>
-                {homePage.homePageData?.electricCategories
-                    ? <ElectronicCategory />
-                    : <pre>{JSON.stringify(homePage.homePageData, null, 2)}</pre>}
-
+                { <ElectronicCategory />}
                 {/* <Banner /> */}
 
 
@@ -55,7 +52,7 @@ const Home = () => {
                         <h1 className=''>
                             Sell Your Product
                         </h1>
-                        <p className='text-lg md:text-2xl'>With <strong className='logo text-3xl md:text-5xl pl-2'>HappyShoping</strong></p>
+                        <p className='text-lg md:text-2xl'>With <strong className='logo text-3xl md:text-5xl pl-2'>HappyShopin</strong></p>
 
                         <div className='pt-6 flex justify-center'>
                             <Button
