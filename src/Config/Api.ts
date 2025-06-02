76b538d12998bb69c3bc8586a7aa4ada9165a713
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2025-06-02 23:28:07 +0530

    api update

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:4000";
+// export const API_URL = "http://localhost:4000";
 export const DEPLOYED_URL = "https://backend-spring-boot-m6de.onrender.com"
-// export const API_URL= "https://zosh-bazzar-backend.onrender.com"
+export const API_URL= "https://backend-spring-boot-m6de.onrender.com"
 // change api
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
